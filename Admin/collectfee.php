8038de3da25f965ec3e5185491c720c3c3fc121e
Prajwalchyy <129585122+Prajwalchyy@users.noreply.github.com>
2024-09-18 15:06:35 +0545

    add pegination

--- Admin/collectfee.php
+++ Admin/collectfee.php
@@ -1,8 +1,51 @@
-<?php include 'db/connection.php';
+<?php 
+include 'db/connection.php';
 
 // Fetch 
 $fetch_programnames = "SELECT * FROM program ORDER BY pname ASC";
 $program_result = mysqli_query($conn, $fetch_programnames);
+
+
+
+$where = "";
+if (isset($_GET['search']) && !empty($_GET['search'])) {
+    $search = mysqli_real_escape_string($conn, $_GET['search']);
+    $where = "WHERE sname LIKE '%$search%' OR senroll LIKE '%$search%' OR scontact LIKE '%$search%'";
+}
+
+//page table limit
+$default_limit = 10;
+
+$limit = isset($_GET['limit']) ? (int)$_GET['limit'] : $default_limit;
+
+if ($limit < 1) {
+    $limit = $default_limit;
+}
+
+
+
+//limit logic of table
+$page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+if ($page < 1) {
+    $page = 1;
+}
+$offset = ($page - 1) * $limit;
+
+
+
+$counter = ($page - 1) * $limit + 1;
+// $total_result = mysqli_query($conn, "SELECT COUNT(*) as total FROM student $where");
+$total_result = mysqli_query($conn, "SELECT COUNT(*) as total FROM student $where");
+$total_row = mysqli_fetch_assoc($total_result);
+$total_records = $total_row['total'];
+
+$total_pages = ceil($total_records / $limit);
+
+
+$select_student = "SELECT * FROM student
+JOIN program ON student.pid = program.pid
+$where ORDER BY CAST(senroll AS UNSIGNED) DESC LIMIT $limit OFFSET $offset";
+$fetch_students = mysqli_query($conn, $select_student);
 
 
 ?>
@@ -49,7 +92,7 @@ $program_result = mysqli_query($conn, $fetch_programnames);
         <div class="collectfee_table_limit">
             <label>Table Limit</label>
             <form action="" method="GET">
-                <input class="collectfee_limit" type="text" name="limit" value="<?php echo isset($_GET['limit']) ? (int)$_GET['limit'] : 5; ?>">
+                <input class="collectfee_limit" type="text" name="limit" value="<?php echo isset($_GET['limit']) ? (int)$_GET['limit'] : 10; ?>">
                 <button type="submit">SET</button>
                 <button type="button" onclick="window.location.href='collectfee.php';">RESET</button>
             </form>
@@ -70,18 +113,40 @@ $program_result = mysqli_query($conn, $fetch_programnames);
                     </tr>
                 </thead>
                 <tbody>
+                <?php
+                    while ($row = mysqli_fetch_assoc($fetch_students)) {
+                    ?>
                     <tr>
-                        <td>1</td>
-                        <td>Apple</td>
-                        <td>0099887766</td>
-                        <td>BCA</td>
+                        <td><?php echo $counter; ?></td>
+                        <td><?php echo $row['sname']; ?></td>
+                        <td><?php echo $row['scontact']; ?></td>
+                        <td><?php echo $row['pname']; ?></td>
                         <td>10000</td>
                         <td>1000</td>
                         <td><button>Collect Fee</button></td>
                     </tr>
+                    <?php
+                        $counter++;
+                    }
+                    ?>
                 </tbody>
             </table>
         </div>
+
+        <div class="studentlist_pagination">
+            <?php if ($page > 1) : ?>
+                <a href="?page=<?php echo $page - 1; ?>&search=<?php echo isset($_GET['search']) ? urlencode($_GET['search']) : ''; ?>&limit=<?php echo $limit; ?>">Previous</a>
+            <?php endif; ?>
+
+            <?php for ($i = 1; $i <= $total_pages; $i++) : ?>
+                <a href="?page=<?php echo $i; ?>&search=<?php echo isset($_GET['search']) ? urlencode($_GET['search']) : ''; ?>&limit=<?php echo $limit; ?>" <?php if ($i == $page) echo 'class="active"'; ?>><?php echo $i; ?></a>
+            <?php endfor; ?>
+
+            <?php if ($page < $total_pages) : ?>
+                <a href="?page=<?php echo $page + 1; ?>&search=<?php echo isset($_GET['search']) ? urlencode($_GET['search']) : ''; ?>&limit=<?php echo $limit; ?>">Next</a>
+            <?php endif; ?>
+        </div>
+
     </div>
 </body>
 
